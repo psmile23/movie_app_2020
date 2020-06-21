6c46f36a8b4c638b7368864ed059bcc8332fdf00
parkse <psmile1023@gmail.com>
2020-06-22 00:29:35 +0900

    jsx_prop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, linkUrl, target}){
+return <h1><a href={linkUrl} target={target}>I love {name}</a></h1>
+}
+
+const FoodList = [
+    { id:'1'
+      , name: 'kimchi'
+      , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.RLV_AfbZ1rzqlYJe-VjzhAHaE_%26pid%3DApi&f=1'
+      , target: '_blank'
+    }
+    ,{
+      id:'2'
+      , name: 'mandoo'
+      , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Rfy3lvvL7MYMn1IA73Ek4QHaEK%26pid%3DApi&f=1'
+      , target: '_blank'
+    }
+    ,{
+      id:'3'
+      , name: 'salad'
+      , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Pslc4lqs2hf0NWIR2A6WpgHaEK%26pid%3DApi&f=1'
+      , target: '_self'
+    }
+    ,{
+      id:'4'
+      , name: 'pizza'
+      , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.j3Awq9aUKyMu2-OQ-WU1OQHaE3%26pid%3DApi&f=1'
+      , target: '_self'
+    }
+];
+
+function renderFunc(obj){
+    return <Food key={obj.id} name={obj.name} linkUrl={obj.linkUrl} target={obj.target}/>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {FoodList.map(renderFunc)}
     </div>
   );
 }
