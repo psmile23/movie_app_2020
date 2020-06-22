4a966d8d529dfb3e50f1cf5d2b5214a58368f38b
parkse <psmile1023@gmail.com>
2020-06-22 20:27:25 +0900

    class componet

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, linkUrl, target, rating }) {
-  return (
-    <div>
-      <img src={linkUrl} alt={name} />
-      <h1><a href={linkUrl} target={target}>I love {name}</a></h1>
-      <h4>{rating}/5.0</h4>
-    </div>
-  )
-}
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
-const FoodList = [
-  {
-    id: '1'
-    , name: 'kimchi'
-    , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.RLV_AfbZ1rzqlYJe-VjzhAHaE_%26pid%3DApi&f=1'
-    , target: '_blank'
-    , rating: 5
+  plus = ()=>{
+    this.setState(current=>({count: current.count+1}));
   }
-  , {
-    id: '2'
-    , name: 'mandoo'
-    , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Rfy3lvvL7MYMn1IA73Ek4QHaEK%26pid%3DApi&f=1'
-    , target: '_blank'
-    , rating: 4.3
+  minus = ()=>{
+    this.setState(current=>({count: current.count-1}));
   }
-  , {
-    id: '3'
-    , name: 'salad'
-    , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Pslc4lqs2hf0NWIR2A6WpgHaEK%26pid%3DApi&f=1'
-    , target: '_self'
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
-  , {
-    id: '4'
-    , name: 'pizza'
-    , linkUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.j3Awq9aUKyMu2-OQ-WU1OQHaE3%26pid%3DApi&f=1'
-    , target: '_self'
-    , rating: 3.2
-  }
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired
-  , linkUrl: PropTypes.string.isRequired
-  , target: PropTypes.string.isRequired
-  , rating: PropTypes.number
-};
-
-function renderFunc(obj) {
-  return <Food key={obj.id} 
-              name={obj.name} 
-              linkUrl={obj.linkUrl} 
-              target={obj.target} 
-              rating={obj.rating}
-          />
-}
-
-function App() {
-  return (
-    <div className="App">
-      {FoodList.map(renderFunc)}
-    </div>
-  );
 }
 
 export default App;
