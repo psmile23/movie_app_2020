2d646b73580d528fd90ff5da7abdc291855e03ae
parkse <psmile1023@gmail.com>
2020-06-23 00:09:27 +0900

    render html_apply

--- src/App.js
+++ src/App.js
@@ -21,16 +21,24 @@ class App extends React.Component{
 
   render(){
     const { isLoading, movies } = this.state;
-    return <div>
-      {isLoading ? "Loading..." : movies.map(movie => {
-        return <Movie key={movie.id}
-                    id={movie.id} 
-                    year={movie.year} 
-                    title={movie.title} 
-                    summary={movie.summary} 
-                    poster={movie.medium_cover_image} />
-      })}
-    </div>
+    return <section class="container">
+      {isLoading ? (
+        <div class="loader"> 
+          <span class="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => {
+            return <Movie key={movie.id}
+                        id={movie.id} 
+                        year={movie.year} 
+                        title={movie.title} 
+                        summary={movie.summary} 
+                        poster={movie.medium_cover_image} />
+          })}
+        </div>
+      )}
+    </section>
   }
 }
 
